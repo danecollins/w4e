a47541854a5224dd541a641a3df2d9972b13c103
Dane Collins <dane@pobox.com>
2015-06-29 20:28:22 -0700

    Fix up urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
-import registration
+from sentinel import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^sentinels/', include('sentinel.urls')),
+    url(r'^checkin/(?P<tag>.*)[/]*$', views.checkin),
+    url(r'^events/history/$', views.event_history),
 )
--- sentinel/urls.py
+++ sentinel/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import patterns, url
 from sentinel import views
 
 urlpatterns = patterns('',
-                       url(r'^events/$', views.event_history),
-                       url(r'^checkin/(?P<tag>.*)[/]*$', views.checkin),
                        url(r'^list/$', views.list),
                        url(r'^add/$', views.add),
                        url(r'^details/(?P<id>[\d]+)/$', views.detail),
