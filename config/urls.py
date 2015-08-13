4de775338c6971ac9e4bbad6309c5e5afd692159
Dane Collins <dane@pobox.com>
2015-08-13 09:48:13 -0700

    Add FAQ

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from sentinel import views
 
 
@@ -14,4 +15,5 @@ urlpatterns = patterns('',
     url(r'^sentinels/', include('sentinel.urls')),
     url(r'^checkin/(?P<tag>.*)[/]*$', views.checkin),
     url(r'^events/history/$', views.event_history),
+    url(r'^faq/', TemplateView.as_view(template_name="faq.html"))
 )
